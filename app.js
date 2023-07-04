0c15be11738abfc3025052d915c4060325c1ae29
rayhans358 <rayhans358@gmail.com>
2023-07-04 21:48:32 +0700

    Task ExpressJs

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.use(express.json);
 app.use('/public', express.static(path.join(__dirname, 'uploads')));
 app.use(router);
 
-// Menagatasi error 404
 app.use((req, res, next) => {
   res.status(404);
   res.send({
