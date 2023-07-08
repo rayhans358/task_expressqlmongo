02ff9a26d4196880a321871335a7d395fd9f1c5d
rayhans358 <rayhans358@gmail.com>
2023-07-09 01:56:00 +0700

    Express & Mysql

--- app/product_v2/controller.js
+++ app/product_v2/controller.js
@@ -5,7 +5,6 @@ const fs = require('fs');
 const upload = multer({dest: '../../uploads'});
 
 const getIndex = async (req, res) => {
-  const { name } = req.query;
   try {
     const products = await Product.findAll();
     res.send(products);
@@ -22,7 +21,7 @@ const getView = async (req, res) => {
     if (product) {
       res.send(product);
     } else {
-      res.status(404).json({
+      res.json({
         message: 'Product not found'
       });
     }
@@ -32,29 +31,87 @@ const getView = async (req, res) => {
 }
 
 const storePost = async (req, res) => {
-  const {users_id, name, price, stock, status} = req.body;
-  console.log(req.body);
+  const { users_id, name, price, stock, status } = req.body;
   const image = req.file;
-  console.log(req.file);
-  if (image) {
-    const target = path.join(__dirname, '../../uploads', image.originalname);
-    fs.renameSync(image.path, target);
-    try{
-      await Product.sync();
-      const result = await Product.create({users_id, name, price, stock, status, image_url: `http://localhost:3000/public/${image.originalname}`});
-      res.send(result);
-    } catch (err) {
-      res.send(err);
+  
+  try {
+    if (!users_id || !name || !price || !stock || !status) {
+      res.json({
+        message: 'Product not complete'
+      });
+    } else {
+      if (image) {
+        const target = path.join(__dirname, '../../uploads', image.originalname);
+        fs.renameSync(image.path, target);
+        
+        await Product.sync();
+        const result = await Product.create({
+          users_id,
+          name,
+          price,
+          stock,
+          status,
+          image_url: `http://localhost:3000/public/${image.originalname}`
+        });
+        
+        res.send(result);
+      } else { //apabila image tidak tersedia 
+        await Product.sync();
+        const result = await Product.create({
+          users_id,
+          name,
+          price,
+          stock,
+          status
+        });
+        res.send(result);
+      }
     }
-  } else {
-    res.json({
-      msg: 'Failed'
-    })
+  } catch (err) {
+    res.send(err);
+  }
+}
+
+const putUpdate = async (req, res) => {
+  const productsId = req.params.id;
+  const { name, price, stock, status } = req.body;
+  const image = req.file;
+
+  try {
+    const product = await Product.findByPk(productsId);
+    if (product) {
+      if (image) {
+        const target = path.join(__dirname, '../../uploads', image.originalname);
+        fs.renameSync(image.path, target);
+        await product.update({
+          name,
+          price,
+          stock,
+          status,
+          image_url: `http://localhost:3000/public/${image.originalname}`
+        });
+      } else {
+        await product.update({
+          name,
+          price,
+          stock,
+          status
+        });
+      }
+      res.send(product);
+    } else {
+      res.json({
+        message: 'Product not found'
+      });
+    }
+  } catch (err) {
+    res.send(err);
   }
 }
 
 module.exports = {
   getIndex,
   getView,
+  putUpdate,
   storePost
 }
--- app/product_v2/routes.js
+++ app/product_v2/routes.js
@@ -8,7 +8,7 @@ const productController = require('./controller');
 router.get('/product', productController.getIndex);
 router.get('/product/:id', productController.getView);
 router.post('/product', upload.single ('image'), productController.storePost);
-// router.put('/product/:id', upload.single('image'),productController.putUpdate);
+router.put('/product/:id', upload.single('image'),productController.putUpdate);
 // router.delete('/product/:id', upload.single('image'),productController.delDestroy);
 
 module.exports = router;
